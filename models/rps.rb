0d29b5ad71fba3ad8d2c26942c8a73a376ebba48
GillianC <gccc0309@gmail.com>
2018-09-25 08:30:44 +0100

    tried to refactor the game, failed but commented attempts out

--- models/rps.rb
+++ models/rps.rb
@@ -4,7 +4,38 @@ class RockPaperScissors
     @hand2 = hand2
   end
 
-  def find_result
+#rock only beats scissors
+#paper only beats rock
+#Scissors only beats paper
+
+#rock beats scissor beats paper beats rock (or draw)
+#rock = 1, scissor = 2, paper = 3
+#
+# Another theory -I think I should be able to do something with an array but I cant figure it out yet
+#
+# if @hand1==@hand2 "Its a draw"
+#
+# options = ["rock", "scissor", "paper"]
+#
+# if @hand1.options.index < @hand2.options.index && #need to cover off rock, paper
+#   @hand1 = winner
+
+
+#tried to simplify this but it doesn't work unless I can find a way to find out what each hand plays.
+  # def find_result
+  #   if (@hand1 || @hand2) == "Rock"
+  #     if (@hand1 || @hand2) == "Scissors"
+  #       "Rock Wins"
+  #     elsif (@hand1 || @hand2) == "Paper"
+  #       "Paper Wins"
+  #     else
+  #       "It's a  draw"
+  #     end
+  #   elsif (@hand1 && @hand2) == "Scissors"
+  #     "It's a draw"
+  #   else
+  #     "Scissors Wins"
+  #   end
 
 
     if ((@hand1 == "Rock") && (@hand2 == "Scissors")) ||((@hand2 == "Rock") && (@hand1 == "Scissors"))
